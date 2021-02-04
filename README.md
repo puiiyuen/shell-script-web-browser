b7404b1f25597a6c27c6f128f032365d51236edd
puiiyuen <peiyuan.chen@smu.ca>
2021-02-03 23:48:36 -0500

    fix HTTP request

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# shell-script-web-browser
+# Shell Script Web Browser
+* This program is a shell web browser
+* Run on Liunx/Mac OS
+* Provide download feature only
--- browser-mac.sh
+++ browser-mac.sh
@@ -1,5 +1,5 @@
 # This program is a shell web browser
-# Run on Linux
+# Run on Mac OS
 # Author: (Patrick) Peiyuan Chen
 
 # ***Start of the program***
@@ -28,10 +28,10 @@ echo "Input a file name for saving new file:" # Create a name for saving new fil
 read fileName
 
 # Establish TCP connection and send HTTP GET request
-echo -e "GET $directory" | nc $IP $port > $fileName # Write the received data to a file
+echo "GET $directory" | nc $IP $port > $fileName # Write the received data to a file
 
 echo "Opening \"$fileName\" ..."
-xdg-open $fileName # Open the file with default application
+open $fileName # Open the file with default application
 
 echo "End of the program"
 
